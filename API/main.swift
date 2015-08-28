98f63c879697af079ac2e9da4d7263a9b0efa3b9
Quentin Pre <pre.quentin@gmail.com>
2015-08-28 08:12:55 +0200

    [ADD] basic XCode project

---
+++ API/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  API
+//
+//  Created by Quentin Pré on 28/08/15.
+//  Copyright © 2015 Quentin Pré. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
