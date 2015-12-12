3b732138cab7bfa7d82ba5a329dba979839078ba
Quentin Pre <pre.quentin@gmail.com>
2015-12-12 10:52:06 +0100

    [ADD] reverting to the original one

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "PhuboAPI",
   dependencies: [
-    .Package(url: "https://github.com/julien-c/swifter.git", majorVersion: 1),
+    .Package(url: "https://github.com/glock45/swifter.git", majorVersion: 1),
   ]
 )
