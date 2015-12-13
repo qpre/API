edcd4cc0fcada3b89c2ea5f89ff1f48cf2601654
Quentin Pre <pre.quentin@gmail.com>
2015-12-13 09:11:36 +0100

    [UPD] should now be supported on heroku

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "PhuboAPI",
   dependencies: [
-    .Package(url: "https://github.com/julien-c/swifter.git", majorVersion: 1),
+    .Package(url: "https://github.com/qpre/swifter.git", majorVersion: 1),
   ]
 )
