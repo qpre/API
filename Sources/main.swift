e0ddafb7097e396b2243b8405c366a19ed660d16
Quentin Pre <pre.quentin@gmail.com>
2015-12-21 10:55:07 +0100

    debug

--- Sources/main.swift
+++ Sources/main.swift
@@ -15,9 +15,15 @@ srandom(UInt32(clock()))
 import Foundation
 import Octopus
 
+print("starting Phubo API")
+
 do {
   let arguments   = Process.arguments
+
+  print("port: String -> Int")
   let port        = Int(arguments[1])
+
+  print("instantiating new OctopusServer")
   let server      = OctopusServer(port: in_port_t(port!))
 
   try server.start()
