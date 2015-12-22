95e64385325ce4b969820cac8a78cfda4c246fec
Quentin Pre <pre.quentin@gmail.com>
2015-12-22 15:28:21 +0100

    [UPD] stop doing shit

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,31 +6,24 @@
 //  Copyright © 2015 Quentin Pré. All rights reserved.
 //
 
+#if os(Linux)
+  import Glibc
+  srandom(UInt32(clock()))
+#endif
+
 import Foundation
 import Octopus
 
-#if os(Linux)
-  import Glibc
-#endif
-
-print("starting Phubo API")
-
 do {
   let arguments   = Process.arguments
-
-  print("port: String -> Int")
   let port        = Int(arguments[1])
-
-  print("instantiating new OctopusServer")
   let server      = OctopusServer(port: port!)
 
   try server.start()
 
-  while (true) {
+  while true {
     sleep(1)
   }
-
-  print("")
 } catch {
     print("Failed at starting Phubo API")
 }
