236c45aa34e229d1685c81a5a27a9d101a3e6533
Quentin Pre <pre.quentin@gmail.com>
2016-01-02 22:16:19 +0100

    [UPD] simplifying

--- Sources/users.swift
+++ Sources/users.swift
@@ -3,10 +3,8 @@ import Octopus
 
 func attachUsersRoutes(router: Octopus.Router) {
   router.get("/users") { (req, res) -> Octopus.HTTPResponse in
-    let json: JSON = ["0": ["name": "quentin", "lastname": "pré"]]
-
     res.contentType = "application/json; charset=utf-8"
-    res.payload     = String(json)
+    res.payload     = String(["0": ["name": "quentin", "lastname": "pré"]] as JSON)
 
     return res
   }
