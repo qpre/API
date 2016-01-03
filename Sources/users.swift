5c42421a98cde862d12ec00180bd9dfe4ea1d1c3
Quentin Pre <pre.quentin@gmail.com>
2016-01-03 22:03:57 +0100

    [ADD] users and user routes

--- Sources/users.swift
+++ Sources/users.swift
@@ -2,9 +2,26 @@ import JSON
 import Octopus
 
 func attachUsersRoutes(router: Octopus.Router) {
+  /*
+  ** GET users
+  ** sends the whole list of users
+  **
+  ** TODO: please paginate...
+  */
   router.get("/users") { (req, res) -> Octopus.HTTPResponse in
     res.contentType = "application/json; charset=utf-8"
     res.payload     = String(["0": ["name": "quentin", "lastname": "pré"]] as JSON)
+
+    return res
+  }
+
+  /*
+  ** GET user
+  ** sends the informations for one specific user
+  */
+  router.get("/users/:id") { (req, res) -> Octopus.HTTPResponse in
+    res.contentType = "application/json; charset=utf-8"
+    res.payload     = String(["name": "quentin", "lastname": "pré"] as JSON)
 
     return res
   }
